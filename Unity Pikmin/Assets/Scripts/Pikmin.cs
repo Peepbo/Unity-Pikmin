6abc8941fbcc6525d007231026fcd00da9b08e89
Peepbo <tngus10778@gmail.com>
2021-02-15 22:32:19 +0900

    PlayerController, Pikmin make script

---
+++ Unity Pikmin/Assets/Scripts/Pikmin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public enum PikminState
+{
+    STAY,
+    FOLLOW,
+    ATTACK
+}
+
+public class Pikmin : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    public bool isAvoid = false;
+    public PikminState state;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        state = PikminState.STAY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case PikminState.STAY:
+                break;
+            case PikminState.FOLLOW:
+                {
+                    float distance = Vector3.Magnitude(target.position - transform.position);
+
+                    if (distance < 1.5f)
+                        isAvoid = true;
+                    else
+                        isAvoid = false;
+
+                    if (!isAvoid)
+                    {
+                        agent.stoppingDistance = 5f;
+                        agent.isStopped = false;
+                        agent.SetDestination(target.position);
+                    }
+                    else
+                    {
+                        Vector3 angle = (transform.position - target.position).normalized;
+                        agent.stoppingDistance = 0.75f;
+                        agent.SetDestination(transform.position + angle);
+                    }
+                }
+                break;
+            case PikminState.ATTACK:
+                break;
+        }
+    }
+}
---
+++ Unity Pikmin/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject circle;
+    float circleRadius = 0;
+
+    public List<Pikmin> pikmins = new List<Pikmin>();
+    public int myPikminCount = 0;
+
+    private void Start()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Pikmin");
+        int len = obj.Length;
+        for(int i = 0; i < len; i++)
+        {
+            pikmins.Add(obj[i].GetComponent<Pikmin>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        //
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 _point = hit.point;
+
+                CircleActive(_point);
+
+                foreach (Pikmin pik in pikmins)
+                {
+                    if(Vector3.Distance(pik.transform.position, _point) < circleRadius)
+                    {
+                        if (pik.state != PikminState.FOLLOW)
+                        {
+                            pik.state = PikminState.FOLLOW;
+                            myPikminCount++;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            CircleDeActive();
+        }
+    }
+
+    void CircleActive(Vector3 pos)
+    {
+        circle.transform.position = pos;
+
+        if (circleRadius > 5.4f) return;
+        circle.SetActive(true);
+        circleRadius = Mathf.Lerp(circleRadius, 5.5f, Time.deltaTime * 3f);
+        float diameter = circleRadius * 2;
+        circle.transform.localScale = Vector3.one * diameter;
+    }
+
+    void CircleDeActive()
+    {
+        if(!circle.activeSelf)return;
+
+        circle.transform.localScale = Vector3.zero;
+        circleRadius = 0;
+        circle.SetActive(false);
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+
+        pos.x += h * Time.deltaTime * 5f;
+        pos.z += v * Time.deltaTime * 5f;
+
+        transform.position = pos;
+    }
+}
