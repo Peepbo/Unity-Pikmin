a9abf7c5f8ea1b5f411ddb582ea5016a8102a6f1
Peepbo <tngus10778@gmail.com>
2021-05-04 00:59:53 +0900

    CalculateVelocity (projectile motion formula, 발사체 운동 공식) 내부 주석 추가

--- Unity Pikmin/Assets/Scripts/Framework/Utils.cs
+++ Unity Pikmin/Assets/Scripts/Framework/Utils.cs
@@ -44,21 +44,43 @@ static class Utils
     //return vector3, get vector to fly with a parabolic
     public static Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
     {
-        //define the distance x and y first
+        //벡터의 뺄샘을 하여 거리, 방향을 저장
         Vector3 distance = target - origin;
+
+        //y를 0으로 두고, xz의 거리, 방향을 저장
         Vector3 distanceXZ = distance;
         distanceXZ.y = 0;
 
+        //높이
         float Sy = distance.y;
+
+        //높이를 무시한 두 벡터간의 거리
         float Sxz = distanceXZ.magnitude;
 
+        /*
+         * xz 속도
+         * x = VelocityXZ * time
+         * VelocityXZ = x / time
+         */
         float Vxz = Sxz / time;
+
+        /*
+         * y 속도
+         * y = Vy * t - 0.5f * g * t²
+         * Vy * t = y + 0.5f * g * t²
+         * Vy = y/t + 0.5f * g * t
+         */
         float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
 
+        //result에 distance를 정규화 한 방향
         Vector3 result = distanceXZ.normalized;
+
+        //xy 속도를 result에 곱
         result *= Vxz;
+        //y 속도를 result의 y에 대입
         result.y = Vy;
 
+        //속도를 담은 vector를 반환
         return result;
     }
 }
